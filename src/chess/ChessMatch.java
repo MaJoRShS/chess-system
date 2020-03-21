03987a9f9ade4f21ddd1a87ad3536bfceaf44d20
MaJoRShS <lukas.mr@hotmail.com>
2020-03-20 21:01:23 -0300

    Little improvement in board printing
    Mudamos as cores de apresentação das peças e precisamos executar nosso programa pelo terminal para poder ver o resultado desejado

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -53,11 +53,24 @@ public class ChessMatch {
 	 * da "ChessPosition" eu to convertendo essa posição para a de matriz o que vai
 	 * colocar a peça no lugar correto, e não crio nada no board porque ele ta no
 	 * método que faz a conversão da parada
+	 * 
+	 * 
+	 * Mandou mais umas peças
 	 */
 	private void initialSetup() {
-		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 8, new King(board, Color.BLACK));
-		placeNewPiece('e', 1, new King(board, Color.BLACK));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
 
 }
